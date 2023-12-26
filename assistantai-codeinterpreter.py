68db91d01d96dd0f49afda03b36777889efcb34a
Martina Ivanicova <martina.ivanicova@kiwi.com>
2023-12-26 22:37:08 +0100

    init commit

---
+++ assistantai-codeinterpreter.py
@@ -0,0 +1,84 @@
+
+# export OPENAI_API_KEY={your key}
+
+from openai import OpenAI, Image
+import time
+from io import BytesIO
+from google.cloud import bigquery
+from PIL import Image
+
+# this part optional
+# prerequisite is having a GCP account, authenticate by running gcloud auth application-default login
+# we use data from the public BigQuery dataset about Austin crime incidents and save it to a csv file
+# alternatively, you can upload your own data to the csv file and use it in the code interpreter
+
+project = 'bi-sandbox-f64c11b3' # your Google Cloud Platform Project ID 
+location = 'US' 
+client = bigquery.Client(project=project, location=location)
+
+# query data
+query = """
+SELECT * FROM `bigquery-public-data.austin_crime.crime`  
+"""
+# Create a BigQuery job
+job = client.query(query)
+
+df = job.to_dataframe()
+df.to_csv("df.csv")
+
+client = OpenAI()
+
+file = client.files.create(
+  file=open("df.csv", "rb"),
+  purpose='assistants'
+)
+
+assistant = client.beta.assistants.create(
+    name="Data Analyses Tutor",
+    instructions="You are my personal data analyses tutor. You will help me analyze data and derive insights from it. Show me the code of your analyses.",
+    tools=[{"type": "code_interpreter"}],
+    model="gpt-4-1106-preview",
+    file_ids=[file.id]
+)
+
+thread = client.beta.threads.create()
+
+print(thread.id)
+
+ # example prompt: Attached is a dataset concerning crime incidents in Austin. Please share any interesting insights you derive from this data. Plot me the output
+
+while True:
+  user_input=input("Type your request to generate another response or 'exit' to end: ")
+  if user_input.lower() == 'exit':
+    break
+  else:
+    message = client.beta.threads.messages.create(thread_id=thread.id, role="user", content=user_input)
+    run = client.beta.threads.runs.create(
+      thread_id=thread.id,
+      assistant_id=assistant.id,
+      instructions="")
+    print(run)
+  while run.status != "completed":
+    run = client.beta.threads.runs.retrieve(
+      thread_id=thread.id,
+      run_id=run.id
+    )
+    time.sleep(3)
+    print(run.status)
+  messages = client.beta.threads.messages.list(
+    thread_id=thread.id
+  )
+  for message in messages.data:
+    if message.run_id != run.id:
+      continue
+    if message.content[0].type == "text":
+      print(message.content[0].text.value)
+    elif message.content[0].type == "image_file":
+      response_file = client.files.with_raw_response.content(message.content[0].image_file.file_id)
+      bytes_io=BytesIO(response_file.content)
+      image = Image.open(bytes_io)
+      if image.mode == 'RGBA':
+        image = image.convert('RGB')
+      image.save("output_image.jpg")
+
+
---
+++ codeinterpreter-api.py
@@ -0,0 +1,116 @@
+#export OPENAI_API_KEY={your key}
+
+from google.cloud import bigquery
+from google_auth_oauthlib import flow
+
+import json
+from langchain.agents.output_parsers import OpenAIFunctionsAgentOutputParser
+from json import JSONDecodeError
+from typing import List, Union
+from langchain_core.exceptions import OutputParserException
+from langchain_core.messages import (
+    AIMessage,
+    BaseMessage,
+)
+from langchain_core.agents import AgentAction, AgentActionMessageLog, AgentFinish
+from codeinterpreterapi import CodeInterpreterSession, settings, File
+
+
+# this part optional 
+# we download data from the public BigQuery dataset about Austin crime incidents and save it to a csv file
+# alternatively, you can upload your own data to the csv file and use it in the code interpreter
+
+project = 'bi-sandbox-f64c11b3' # your Google Cloud Platform Project ID 
+location = 'US' # Location inserted based on the query results selected to explore
+client = bigquery.Client(project=project, location=location)
+
+# query data
+query = """
+SELECT * FROM `bigquery-public-data.austin_crime.crime`  
+"""
+# Create a BigQuery job
+job = client.query(query)
+
+df = job.to_dataframe()
+df.to_csv("df.csv")
+
+
+# monkeypatched method which fixes the problem with json.loads with unescaped newlines \n -> \\n
+# original: https://api.python.langchain.com/en/latest/_modules/langchain/agents/output_parsers/openai_functions.html
+def my_parse_ai_message(message: BaseMessage) -> Union[AgentAction, AgentFinish]:
+    """Parse an AI message. Add json.dumps before doing json.loads to fix escaping errors"""
+    if not isinstance(message, AIMessage):
+        raise TypeError(f"Expected an AI message got {type(message)}")
+
+    function_call = message.additional_kwargs.get("function_call", {})
+
+    if function_call:
+        function_name = function_call["name"]
+        try:
+            if len(function_call["arguments"].strip()) == 0:
+                # OpenAI returns an empty string for functions containing no args
+                _tool_input = {}
+            else:
+                # otherwise it returns a json object
+                _tool_input = json.dumps(function_call["arguments"])  # This line was added to the original function
+                _tool_input = json.loads(_tool_input)
+        except JSONDecodeError:
+            raise OutputParserException(
+                f"Could not parse tool input: {function_call} because "
+                f"the `arguments` is not valid JSON."
+            )
+
+        # HACK HACK HACK:
+        # The code that encodes tool input into Open AI uses a special variable
+        # name called `__arg1` to handle old style tools that do not expose a
+        # schema and expect a single string argument as an input.
+        # We unpack the argument here if it exists.
+        # Open AI does not support passing in a JSON array as an argument.
+        if "__arg1" in _tool_input:
+            tool_input = _tool_input["__arg1"]
+        else:
+            tool_input = _tool_input
+
+        content_msg = f"responded: {message.content}\n" if message.content else "\n"
+        log = f"\nInvoking: `{function_name}` with `{tool_input}`\n{content_msg}\n"
+        return AgentActionMessageLog(
+            tool=function_name,
+            tool_input=tool_input,
+            log=log,
+            message_log=[message],
+        )
+
+    return AgentFinish(
+        return_values={"output": message.content}, log=str(message.content)
+    )
+
+OpenAIFunctionsAgentOutputParser._parse_ai_message = my_parse_ai_message
+
+with CodeInterpreterSession(verbose=True) as session:
+    user_request = """
+      Attached is a dataset concerning crime incidents in Austin. Please share any interesting insights you derive from this data.          
+     """
+    while True:
+        files = [
+            # attach files to the request
+            File.from_path("df.csv"),
+        ]
+
+        # generate the response
+        response = session.generate_response(
+            user_request, files=files
+        )
+
+        # output to the user
+        print("AI: ", response.content)
+        for file in response.files:
+            # iterate over the files (display if image)
+            file.show_image()
+
+        # Wait for user input to continue or break the loop
+        cont = input("Type your request to generate another response or 'exit' to end: ")
+        if cont.lower() == 'exit':
+            break
+        else:
+            user_request = cont
+
